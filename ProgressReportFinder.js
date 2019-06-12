524eeb71c75d9114572ade711fa4f5ee275514af
James Chen <jchen3652@gmail.com>
2019-06-12 13:23:48 -0400

    Add no report generated case

--- ProgressReportFinder.js
+++ ProgressReportFinder.js
@@ -1,12 +1,14 @@
-var start = 8500; // Recommended: 8500
-var end = 9750; // Recommended: 9600
+var start = 8400; // Recommended: 8400
+var end = 9900; // Recommended: 9900
 var semester = 2;
 
 function getInfo(theUrl) {
   var xmlHttp = new XMLHttpRequest();
   xmlHttp.onreadystatechange = function() {
     if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
-      if (xmlHttp.responseText.includes('Unable To Acquire Required Data For Report')) {} else {
+      if (xmlHttp.responseText.includes('Unable To Acquire Required Data For Report') || xmlHttp.responseText.includes('No report generated')) {
+        // do nothing
+      } else {
         console.log(theUrl);
       }
     }
